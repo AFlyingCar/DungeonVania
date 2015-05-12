b7c07f79e302263f99117c5226e219d850465234
Jacob Meserve <Jsm2017@gmail.com>
2015-05-12 14:04:10 -0700

    First Commit. Basically Done. Can change if needed to.

---
+++ Shop.java
@@ -0,0 +1,101 @@
+/*Jacob Meserve
+*Shop.java
+*5/12/2015
+*Used to buy items for the player
+*/
+
+import java.util.Scanner;
+class Shop{
+	
+	private Player player;
+	private Scanner input;
+	
+
+	public Shop(Player newPlayer){
+		player = newPlayer;
+	}
+
+	public String menu(){
+		System.out.println(player.getName() + ": enters the shop.");
+		System.out.println("Shopkeeper: What do ya need?");
+		System.out.println("0. Exit");
+		System.out.println("1. Buy Health Potions");
+		System.out.println("2. Upgrade Armor");
+		System.out.println("3. Upgrade Weapon");
+		System.out.println("Choice: "):
+		int choice = input.nextInt();
+		if(choice == 0)
+			return(player.getName + ": leaves the store");
+		else if(choice == 1)
+			return buyHealth();
+		else if(choice == 2)
+			return buyArmor();
+		else if(choice == 3)
+			return buyDamage();
+		else{
+			System.out.println(choice + "is not a choice.");
+			return menu();
+		}
+
+	}
+
+	private String buyHealth(){
+		System.out.println("How many would ya like to bye? ");
+		System.out.println("Choice: ");
+		int choice = input.nextInt();
+		int count = 0;
+		if(choice > 0){
+			for(int i = 0; i < choice; i++){
+				if(buyPotion())
+					count++;
+				else
+					break;
+			}
+			return(player.getName() + ": bought " + count + "potion(s)\n" + player.getName() + "'s gold: " + player.getMoney());
+		}else{
+			System.out.println("You cant bye " + choice + " potions");
+			buyHealth();
+		}
+	}
+
+	private String buyArmor(){
+		System.out.println("How many would ya like to bye? ");
+		System.out.println("Choice: ");
+		int choice = input.nextInt();
+		int count = 0;
+		if(choice > 0){
+			for(int i = 0; i < choice; i++){
+				if(buyPotion())
+					count++;
+				else
+					break;
+			}
+			return(player.getName() + ": bought " + count + "armor upgrade(s)\n" + player.getName() + "'s gold: " + player.getMoney());
+			}else{
+				System.out.println("You cant bye " + choice + " armor upgrades");
+				buyHealth();
+			}
+		}
+	}
+
+	private String buyDamage(){
+		System.out.println("How many would ya like to bye? ");
+		System.out.println("Choice: ");
+		int choice = input.nextInt();
+		int count = 0;
+		if(choice > 0){
+			for(int i = 0; i < choice; i++){
+				if(buyPotion())
+					count++;
+				else
+					break;
+			}
+			return(player.getName() + ": bought " + count + "weapon upgrades(s)\n" + player.getName() + "'s gold: " + player.getMoney());
+			}else{
+			System.out.println("You cant bye " + choice + " weapon upgrades");
+			buyHealth();
+			}
+		}
+	}
+
+}
